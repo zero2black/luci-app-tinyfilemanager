c242c93edb2da05ccaff70f9100ad3c7ac037f09
zero2black <101183329+zero2black@users.noreply.github.com>
2024-12-01 06:49:22 +0700

    Update tinyfilemanager.lua

--- luasrc/controller/tinyfilemanager.lua
+++ luasrc/controller/tinyfilemanager.lua
@@ -1,5 +1,4 @@
 module("luci.controller.tinyfilemanager", package.seeall)
 function index()
-	entry({"admin", "services"}, firstchild(), "Services", 99).dependent=false
-	entry({"admin", "services", "tinyfilemanager"}, template("tinyfilemanager"), _("Tiny File Manager"), 99).dependent=true
+	entry({"admin", "services", "tinyfilemanager"}, template("tinyfilemanager"), _("Tiny File Manager"), 50).dependent=true
 end
